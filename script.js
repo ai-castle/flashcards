861b3d4971bd58ee65c3962b7bcb38417bf99fad
dongorae <ldjnewdress@gmail.com>
2023-05-15 14:12:14 +0900

    m

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// Simple list
-// Sortable.create(simpleList, { /* options */ });
 
 // List with handle
 var sortable = Sortable.create(listWithHandle, {
-  handle: '.glyphicon-move',
-  swapThreshold: 1,
+//   handle: '.bi-arrows-move',
   animation: 150,
   onEnd: function(event) {
     console.log('순서가변경됨');
@@ -14,61 +11,110 @@ var sortable = Sortable.create(listWithHandle, {
 });
 
 
-const draggables = document.querySelectorAll(".draggable");
-const containers = document.querySelectorAll(".container");
+///////////////////////////////
+let modalEvent;
+let newNote;
+let noteEditTarget;
+const noteNameModal = document.getElementById('noteNameModal')
 
-draggables.forEach(draggable => {
-  draggable.addEventListener("dragstart", () => {
-    draggable.classList.add("dragging");
-  });
+// // 단어장 추가 
+// const createNote = function (){
+//     // noteNameSample.html 파일을 불러옵니다.
+//     fetch('noteNameSample.html')
+//     .then(response => response.text())
+//     .then(html => {
+//         // 불러온 HTML 코드를 파싱합니다.
+//         const parser = new DOMParser();
+//         const doc = parser.parseFromString(html, 'text/html');
 
-  draggable.addEventListener("dragend", () => {
-    draggable.classList.remove("dragging");
-  });
-});
+//         // noteNameSample.html에서 원하는 요소를 선택합니다.
+//         newNote = doc.querySelector('.card.list-group-item');
 
-containers.forEach(container => {
-  container.addEventListener("dragover", e => {
-    e.preventDefault();
-    const afterElement = getDragAfterElement(container, e.clientX);
-    const draggable = document.querySelector(".dragging");
-    if (afterElement === undefined) {
-      container.appendChild(draggable);
+//         // 부모 요소에 자식 요소로 추가합니다.
+//         const parentElement = document.getElementById('listWithHandle');
+//         parentElement.appendChild(newNote);
+
+//     })
+//     .catch(error => {
+//         console.error('파일을 불러오는 중 오류가 발생했습니다:', error);
+//     });
+
+// }
+
+
+// 단어장 추가
+const createNote = function () {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'noteNameSample.html', false); // 동기식으로 설정
+    xhr.send();
+  
+    if (xhr.status === 200) {
+      // 불러온 HTML 코드를 파싱합니다.
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(xhr.responseText, 'text/html');
+  
+      // noteNameSample.html에서 원하는 요소를 선택합니다.
+      newNote = doc.querySelector('.card.list-group-item');
+  
+      // 부모 요소에 자식 요소로 추가합니다.
+      const parentElement = document.getElementById('listWithHandle');
+      parentElement.appendChild(newNote);
     } else {
-      container.insertBefore(draggable, afterElement);
+      console.error('파일을 불러오는 중 오류가 발생했습니다:', xhr.status);
     }
-  });
-});
+  };
+  
 
-function getDragAfterElement(container, x) {
-  const draggableElements = [
-    ...container.querySelectorAll(".draggable:not(.dragging)"),
-  ];
+// 단어장 추가 모달창 오픈
+const noteCreateModalOpen = function(event){
+    modalEvent = 'noteNameCreate';    
+    // const button = event.relatedTarget
+    // const recipient = button.getAttribute('data-bs-whatever')
+    const modalTitle = noteNameModal.querySelector('.modal-title')
+    const modalBodyInput = noteNameModal.querySelector('.modal-body input')
 
-  return draggableElements.reduce(
-    (closest, child) => {
-      const box = child.getBoundingClientRect();
-      const offset = x - box.left - box.width / 2;
-      // console.log(offset);
-      if (offset < 0 && offset > closest.offset) {
-        return { offset: offset, element: child };
-      } else {
-        return closest;
-      }
-    },
-    { offset: Number.NEGATIVE_INFINITY },
-  ).element;
+    modalTitle.textContent = `단어장 이름 (추가)`
+    modalBodyInput.value = ''
+}
+
+// 단어장 수정 모달창 오픈 
+const noteEditModalOpen = function(event){
+    console.log(event.target);
+    modalEvent = 'noteNameEdit';
+    const modalTitle = noteNameModal.querySelector('.modal-title')
+    const modalBodyInput = noteNameModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `단어장 이름 (수정)`;
+    noteEditTarget = event.target.previousElementSibling;
+    modalBodyInput.value = noteEditTarget.textContent;
 }
 
 
+// 단어장 모달창 입력 완료
+var noteNameDone = document.querySelector('#submit-note-name');
+noteNameDone.addEventListener("click", (event) => {
+    if (modalEvent === 'noteNameEdit') {
+        noteEditTarget.textContent = noteNameModal.querySelector('.modal-body input').value;
+    } else {
+        createNote();
+        newNote.querySelector('h3').textContent = noteNameModal.querySelector('.modal-body input').value;
+        // 단어장 수정 이벤트리스너 연결
+        document.querySelectorAll('.bi-pencil-fill').forEach(edit => {
+            edit.addEventListener("click", noteEditModalOpen)
+        })
+    };
+})
 
 
-var edits = document.querySelectorAll('.glyphicon-pencil');
-console.log(edits)
+// 기존에 있는 단어장 가져오기
+createNote();
+createNote();
 
-edits.forEach(edit => {
-    edit.addEventListener("click", (event) => {
-      let inputText = prompt("단어장 이름", event.target.previousElementSibling.textContent);
-      event.target.previousElementSibling.textContent = inputText
-    });
-});
+
+// 단어장 수정 이벤트리스너 연결
+document.querySelectorAll('.bi-pencil-fill').forEach(edit => {
+    edit.addEventListener("click", noteEditModalOpen)
+})
+
+// 단어장 추가 모달창 오픈 이벤트리스너 연결
+document.querySelector('#add-note').addEventListener("click", noteCreateModalOpen)
